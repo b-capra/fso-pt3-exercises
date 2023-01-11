e274886f4d60d5680ef9ea2966ff32282ba26363
b-capra <caprabmc@gmail.com>
2023-01-11 15:46:41 -0500

    Setup heroku deployment

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ app.post('/api/persons', (request, response) => {
 
 // PORT LISTENER
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
   console.debug('App listening on :3001')
